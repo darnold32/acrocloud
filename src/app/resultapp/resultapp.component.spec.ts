b4b1ee55f75bdfac3b568a21ca0803352315008c
Dereb <A1394358@bestbuy.com>
2018-07-09 16:25:13 -0500

    resultapp (2nd page) created. css updated

---
+++ src/app/resultapp/resultapp.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ResultappComponent } from './resultapp.component';
+
+describe('ResultappComponent', () => {
+  let component: ResultappComponent;
+  let fixture: ComponentFixture<ResultappComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ResultappComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ResultappComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
