b2ee1552fcfa013d4dba34048b97eecd54cc191b
Arnold <A1331761@bestbuy.com>
2018-07-12 08:45:18 -0500

    Saved

--- src/app/acronym-db.service.ts
+++ src/app/acronym-db.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Acronym } from './acronym'
+import { acronyms } from './java-api'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AcronymDbService {
 
-  constructor() { }
+
+  constructor() {}
+
+  findAcronyms(acronym: string) Observable<Acronym[]>{
+
+    return of(acronyms.find(acronym => acronym.name = acronym));
+
+  }
+
+
+  
 }
 
 
