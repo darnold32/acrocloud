9d5a6a0ab7c026c841143735dd91b31528659915
Arnold <A1331761@bestbuy.com>
2018-07-17 16:12:52 -0500

    DummyUrl and Parsing Json Object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {ResultappComponent} from './resultapp/resultapp.component';
+// import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 import { BrowserModule } from '../../node_modules/@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AcronymDbService } from './acronym-db.service';
+import { Acronym } from './acronym';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +23,7 @@ declarations: [
 imports: [
   BrowserModule,
   FormsModule,
+  // HttpModule,
   NgbModule,
 ],
 providers: [AppComponent, AcronymDbService],
@@ -28,17 +33,25 @@ bootstrap: [AppComponent]
 export class AppComponent {
   title = 'Acronym Search';
   value = '';
+  // public acronym:Acronym;
   private apiUrl = "http://localhost:8080";
      data: any = {};
 
   constructor(private service: AcronymDbService) {
         console.log()
-        // service.getAcronym(this.value);
-        // service.getData(this.value);
+
+        // this.acronym = new Acronym();
+        // this.acronym.acronym = "AAA";
+        // this.acronym.name = "Acronym";
+        // this.acronym.description = "Descripton goes here";  
+        // this.acronym.catagory = "Cata"; 
+        
       }
       
       searchAcronyms(value: string) {
         console.log(this.value)
                 this.service.getAcronym(this.value);
-         this.value = value; }
+         this.value = value; 
+        }
  }
+
